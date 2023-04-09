7f3e3de77eb9bcb42c61018cbf2e24bd2ca8eb18
Leonid Vasilev <leonid.vasilev@serokell.io>
2023-04-09 11:36:29 +0400

    Init

---
+++ src/main/java/catalog/domain/DomainEntity.java
@@ -0,0 +1,24 @@
+package catalog.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bson.Document;
+
+@JsonIgnoreProperties("_id")
+public abstract class DomainEntity {
+    public Document toDocument() {
+        return MapperWrapper.oMapper.convertValue(this, Document.class);
+    }
+
+    public static <T extends DomainEntity> T fromDocument(Document document, Class<T> clazz) {
+        return MapperWrapper.fromDocument(document, clazz);
+    }
+
+    private static class MapperWrapper {
+        private static final ObjectMapper oMapper = new ObjectMapper();
+
+        static <T extends DomainEntity> T fromDocument(Document document, Class<T> clazz) {
+            return MapperWrapper.oMapper.convertValue(document, clazz);
+        }
+    }
+}
